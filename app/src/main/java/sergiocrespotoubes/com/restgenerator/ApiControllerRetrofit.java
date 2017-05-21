ddea6c7765e45c876c56120174e11917a2ab3a2b
SergioCT <SergioCrespoToubes@gmail.com>
2017-05-22 00:24:44 +0200

    Tests with volley and retrofit

---
+++ app/src/main/java/sergiocrespotoubes/com/restgenerator/ApiControllerRetrofit.java
@@ -0,0 +1,20 @@
+package sergiocrespotoubes.com.restgenerator;
+
+
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+/**
+ * Created by Sergio on 20-May-17.
+ */
+
+public interface ApiControllerRetrofit {
+
+    @GET("/users")
+    String getData();
+
+    @POST("/posts")
+    String postData(@Field("body") String body);
+
+}
--- app/src/main/java/sergiocrespotoubes/com/restgenerator/MainActivity.java
+++ app/src/main/java/sergiocrespotoubes/com/restgenerator/MainActivity.java
@@ -9,14 +9,32 @@ import com.sergiocrespotoubes.restgenerator.RestGenerator;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_result;
+    TextView tv_result_retrofit;
+    TextView tv_result_volley;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv_result = (TextView) findViewById(R.id.tv_result);
+        tv_result          = (TextView) findViewById(R.id.tv_result);
+        tv_result          = (TextView) findViewById(R.id.tv_result_get);
+        tv_result_retrofit = (TextView) findViewById(R.id.tv_result_retrofit);
+        tv_result_retrofit = (TextView) findViewById(R.id.tv_result_get_retrofit);
+        tv_result_volley   = (TextView) findViewById(R.id.tv_result_volley);
+        tv_result_volley   = (TextView) findViewById(R.id.tv_result_get_volley);
+
+        restGenerator();
+        retrofit();
+        volley();
+    }
+
+    private void restGenerator() {
 
         RestGenerator restGenerator = new RestGenerator("https://jsonplaceholder.typicode.com");
         ApiController apiController = restGenerator.build(ApiController.class);
@@ -40,4 +58,17 @@ public class MainActivity extends AppCompatActivity {
 
         tv_result.setText(result);
     }
+
+    private void retrofit() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .build();
+
+        ApiControllerRetrofit service = retrofit.create(ApiControllerRetrofit.class);
+    }
+
+    private void volley() {
+
+    }
+
 }
